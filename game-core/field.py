b2e3dfccb2268f54212741f9d84f388eb191a8f5
0Franky <frhaexin@gmail.com>
2020-05-11 12:11:12 +0200

    aggiunti commenti + fix
    fix: ship isDestroyed, fire into battleship

--- game-core/field.py
+++ game-core/field.py
@@ -3,9 +3,14 @@ from fire_result import FireResult
 
 class Filed:
 
+  # Init della classe (costruttore)
+  # Params:
+  # # rowLenght: lunghezza delle righe (passata dal core)
+  # # columnLenght: lunghezza delle colonne (passata dal core)
   def __init__(self, rowLenght, columnLenght):
     self.rowLenght = rowLenght
     self.columnLenght = columnLenght
+    # Crea il dataset delle navi disponibili da posizionare
     self.ships = [
       Ship(2, "v", 0, 0), 
       Ship(2, "v", 0, 0), 
@@ -15,15 +20,25 @@ class Filed:
       Ship(6, "v", 0, 0)
     ]
 
+  # Metodo che permette di posizionare una nave 
+  # del dataset all'interno del campo di battaglia
   def shipPositioning(self, indexShip, orientation, startLocationX, startLocationY):
+    # Setto l'orientamento [
+    # # v = verticale ; 
+    # # o = orizzontale ## mai usata -> usato != v
+    # ] : stringa
     self.ships[indexShip].setOrientation(orientation)
+    # posizionamento della nave tramite le coordinate x-y
     self.ships[indexShip].setPosition(startLocationX, startLocationY)
+    # verifico che la posizione sia corretta
     if self.validatePosition(indexShip) == False:
       self.ships[indexShip].place()
     else:
       # return error
-      print("error")
+      print("error") # temporaneamente solo scritta errore
     
+  # Metodo che verifica che la posizione sia corretta:
+  # nessuna collisione o posizionamento oltre il bordo
   def validatePosition(self, indexShip):
     valid = True
     for i in range(len(self.ships)):
@@ -35,7 +50,9 @@ class Filed:
       if valid == False:
         break
     return valid
-    
+  
+  # Metodo che verifica che non ci siano 
+  # collisioni tra le navi (navi sovrapposte)
   def detectCollision(self, indexShip1, indexShip2, recalculateInvers = True):
     valid = True
     startShip1 = self.ships[indexShip1].getStartLocationX()
@@ -56,6 +73,8 @@ class Filed:
       valid = (indexShip1, indexShip2, False)
     return valid
     
+  # Metodo che verifica che la nave 
+  # non vada oltre il bordo del campo
   def detectOutBound(self, indexShip):
     start = self.ships[indexShip].getStartLocationX()
     end = self.columnLenght
@@ -65,8 +84,17 @@ class Filed:
     return start + self.ships[indexShip].getSize() < end
 
   def fire(self, x, y):
-    return self.isHitted(x, y) # ricordo che volevo scrivere qualcos'altro qui ma non ricordo cosa
+    if x >= 0 and x < self.columnLenght and y >= 0 and y < self.rowLenght:
+      return self.isHitted(x, y)
+    else: 
+      return FireResult.BAD_REQUEST_404
 
+  # Verifica se date due coordinate, il colpo colpisce, 
+  # affonda e colpisce una nave, 
+  # avviene un miss oppure 
+  # è una richiesta non valida 
+  # (ad esempio si cerca di colpire 
+  # un punto già colpito)
   def isHitted(self, x, y):
     result = FireResult.MISS
     hitted = False
@@ -89,10 +117,13 @@ class Filed:
         break
     return result
 
+  # Metodo che verifica se si ha perso,
+  # ovvero se le tutte le proprie (posizionate)
+  # navi sono distrutte
   def isLostGame(self):
     isLost = True
     for ship in self.ships:
-      if ship.isDestroyed() == False:
+      if ship.isPlaced() and ship.isDestroyed() == False:
         isLost = False
         break
     return isLost
